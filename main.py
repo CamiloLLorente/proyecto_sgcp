5187310015bbf9bc0a6670ad2596dcc734446e7f
Camilo LLorente <camiloandresllorete@gmail.com>
2020-12-10 22:39:26 -0500

    commit inicial

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from repository import repository_producto
 
 from fastapi import FastAPI, HTTPException
 
-app = FastAPI()
+api = FastAPI()
 
 
-@app.get("/productos")
+@api.get("/productos")
 async def get_productos():
     return producto_db.get_productos()
 
 
-@app.get("/productos/out")
+@api.get("/productos/out")
 async def get_productos():
     return repository_producto.get_productos_out()
 
 
-@app.get("/producto/{cod_producto}")
+@api.get("/producto/{cod_producto}")
 async def get_producto_by_cod(cod_producto: str):
     if producto_db.get_producto(cod_producto) is None:
         raise HTTPException(
@@ -26,19 +26,19 @@ async def get_producto_by_cod(cod_producto: str):
     return producto_db.get_producto(cod_producto)
 
 
-@app.post("/producto/nuevo")
+@api.post("/producto/nuevo")
 async def new_producto(producto: producto_db.ProductoInDB):
     producto_db.database_producto[producto.codigo] = producto
     return producto_db.get_productos()
 
 
-@app.delete("/producto/{cod_producto}")
+@api.delete("/producto/{cod_producto}")
 async def delete_producto(cod_producto: str):
     del producto_db.database_producto[cod_producto]
     return producto_db.get_productos()
 
 
-@app.put("/producto/{producto_id}")
+@api.put("/producto/{producto_id}")
 async def update_producto(producto_id: str, producto: producto_db.ProductoInDB):
     producto_db.database_producto[producto_id] = producto
     return producto_db.get_productos()
